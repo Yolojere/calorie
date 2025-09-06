03196b5db53dd3ddbd61feb12c1824eaf032c04b
Yolojere <jere.huumo@hotmail.com>
2025-09-07 00:02:45 +0300

    Update app.py

--- app.py
+++ app.py
@@ -72,12 +72,17 @@ oauth = OAuth(app)
 # Create OAuth clients
 google = oauth.register(
     name='google',
+    client_id=os.getenv("GOOGLE_CLIENT_ID"),
+    client_secret=os.getenv("GOOGLE_CLIENT_SECRET"),
     server_metadata_url='https://accounts.google.com/.well-known/openid-configuration',
     client_kwargs={'scope': 'openid email profile'}
+
 )
 
 github = oauth.register(
     name='github',
+    client_id=os.getenv("GITHUB_CLIENT_ID"),
+    client_secret=os.getenv("GITHUB_CLIENT_SECRET"),
     access_token_url='https://github.com/login/oauth/access_token',
     authorize_url='https://github.com/login/oauth/authorize',
     api_base_url='https://api.github.com/',
